bea74c7f587575ba65d2eddca05a35dbdc58b6d6
Vandenborne Stefan <stefan@illutek.be>
2014-09-16 21:42:23 +0200

    script commentaar vertaald 16092014

--- js/script.js
+++ js/script.js
@@ -3,24 +3,20 @@
         attach: function(context, settings) {
 
             if ($(".view").hasClass("drupalsite-view")) {
-                $(".view").addClass("grid-view"); // Sets .grid-view as default class on the view
+                $(".view").addClass("grid-view"); // Zet .grid-view als standaard class van de view
                 $(".table").click(function() {
                     if ($(".view").hasClass("grid-view")) {
-                        $(".view").addClass("list-view"); // adds .grid-view to the view class
-                        $(".view").removeClass("grid-view"); // removes .list-view from the view class
+                        $(".view").addClass("list-view"); // voeg .list-view aan de view class
+                        $(".view").removeClass("grid-view"); // verwijder .grid-view van de view class
                     }
                 });
                 $(".list").click(function() {
                     if ($(".view").hasClass("list-view")) {
-                        $(".view").addClass("grid-view"); // adds .list-view to the view class
-                        $(".view").removeClass("list-view"); // removes .grid-view from the view class
+                        $(".view").addClass("grid-view"); // voeg .grid-view van de view class
+                        $(".view").removeClass("list-view"); // verwijder .list-view van de view class
                     }
                 });
             }
-            // //Sets default class for course views and toggles class when changing view.
-            // $('#content').once('drupalsite-view', function() {
-            //     console.log('has it0');
-            // });
         }
     };
 }) (jQuery, Drupal, this, this.document);
